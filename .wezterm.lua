f04859137f2509eff10f6c137a964e417ee32162
StefanoND <sndobbin@gmail.com>
2024-01-24 18:14:53 +0000

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,63 +5,74 @@ local act = wezterm.action
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Begin
-
 config.automatically_reload_config = true
 config.check_for_updates = false
 
-config.leader = { key = "b", mods = "CTRL" }
+-- config.leader = { key = "b", mods = "CTRL" }
+config.leader = { key = "Space", mods = "CTRL" }
 config.keys = {
-	{ key = "(", mods = "CTRL|SHIFT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	{ key = ")", mods = "CTRL|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	{ key = '"', mods = "LEADER|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	{
-		key = "%",
-		mods = "LEADER|SHIFT",
-		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-	},
-	{ key = "T", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{ key = "LeftArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "RightArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "UpArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "DownArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
-	{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
-	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-	{ key = "w", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+  -- { key = "(", mods = "CTRL|SHIFT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+  -- { key = ")", mods = "CTRL|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+  -- { key = '"', mods = "LEADER|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+  -- {
+  --   key = "%",
+  --   mods = "LEADER|SHIFT",
+  --   action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+  -- },
+  -- { key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+  -- { key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+  -- { key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+  -- { key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+  -- { key = "h", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+  -- { key = "j", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+  -- { key = "k", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+  -- { key = "l", mods = "ALT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+  -- { key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+  -- { key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+  -- { key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+  -- { key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+  -- { key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
+  -- { key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+
+  {
+    key = "T",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+  },
+  { key = "LeftArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+  { key = "RightArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+  { key = "UpArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+  { key = "DownArrow", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+  { key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+  { key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+  { key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+  { key = "w", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 }
 
 for i = 1, 8 do
-	-- LEADER (Default: CTRL + b) + number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i - 1),
-	})
-	-- F1 through F8 to activate that tab
-	table.insert(config.keys, {
-		key = "F" .. tostring(i),
-		action = act.ActivateTab(i - 1),
-	})
+  -- LEADER (Default: CTRL + Space) + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1),
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(config.keys, {
+    key = "F" .. tostring(i),
+    action = act.ActivateTab(i - 1),
+  })
 end
 
-config.default_prog = { "bash" }
+-- config.default_prog = { "bash" }
+config.default_prog = { "tmux" }
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 11.0
+config.font_size = 10.0
 config.freetype_load_target = "Normal"
 config.freetype_render_target = "HorizontalLcd"
 config.freetype_load_flags = "FORCE_AUTOHINT"
--- install.sh
+++ install.sh
@@ -10,63 +10,86 @@ ln -svf .bashrc ~/
 if [ -f ~/.bash_aliases ]; then
   mv ~/.bash_aliases ~/.bash_aliases.bak
 fi
-ln -svf .bash_aliases ~/
+ln -svf ~/dotfiles/.bash_aliases ~/
 
 # Bash logout
 if [ -f ~/.bash_logout ]; then
   mv ~/.bash_logout ~/.bash_logout.bak
 fi
-ln -svf .bash_logout ~/
+ln -svf ~/dotfiles/.bash_logout ~/
 
 # Editorconfig
 if [ -f ~/.editorconfig ]; then
   mv ~/.editorconfig ~/.editorconfig.bak
 fi
-ln -svf .editorconfig ~/
+ln -svf ~/dotfiles/.editorconfig ~/
 
 # Git Attributes
 if [ -f ~/.gitattributes ]; then
   mv ~/.gitattributes ~/.gitattributes.bak
 fi
-ln -svf .gitattributes ~/
+ln -svf ~/dotfiles/.gitattributes ~/
 
 # Git Config
 if [ -f ~/.gitconfig ]; then
   mv ~/.gitconfig ~/.gitconfig.bak
 fi
-ln -svf .gitconfig ~/
+ln -svf ~/dotfiles/.gitconfig ~/
 
 # GTKRC-2 Garuda
 if [ -f ~/.gtkrc-2.0.mine ]; then
   mv ~/.gtkrc-2.0.mine ~/.gtkrc-2.0.mine.bak
 fi
-ln -svf .gtkrc-2.0.mine ~/
+ln -svf ~/dotfiles/.gtkrc-2.0.mine ~/
 
 # Profile
 if [ -f ~/.profile ]; then
   mv ~/.profile ~/.profile.bak
 fi
-ln -svf .profile ~/
+ln -svf ~/dotfiles/.profile ~/
 
 # Wezterm config
 if [ -f ~/.wezterm.lua ]; then
   mv ~/.wezterm.lua ~/.wezterm.lua.bak
 fi
-ln -svf .wezterm.lua ~/
+ln -svf ~/dotfiles/.wezterm.lua ~/
 
 # Nvim
 if [ -f ~/.config/stvim ]; then
   mv ~/.config/stvim ~/.config/stvim.bak
 fi
-ln -svf .config/stvim ~/.config/
+ln -svf ~/dotfiles/.config/stvim ~/.config/
 # Nvim
 if [ -f ~/.config/nvchad ]; then
   mv ~/.config/nvchad ~/.config/nvchad.bak
 fi
-ln -svf .config/stvim ~/.config/
+ln -svf ~/dotfiles/.config/nvchad ~/.config/
 
 # i3
 if [ -f ~/.config/i3 ]; then
   mv ~/.config/i3 ~/.config/i3.bak
 fi
-ln -svf .config/i3 ~/.config/
+ln -svf ~/dotfiles/.config/i3 ~/.config/
+
+# tmux
+if [ -d ~/.config/tmux ]; then
+  mv ~/.config/tmux ~/.config/tmux.old
+fi
+mkdir ~/.config/tmux
+ln -svf ~/dotfiles/.config/tmux ~/.config
+
+if ! "$(pacman -Q | grep -iq 'tmux')"; then
+  echo
+  echo 'Installing tmux'
+  echo
+  sudo pacman -S tmux --noconfirm --needed
+  sleep 1s
+fi
+
+if ! [ -f "$HOME"/.tmux/plugins/tpm ]; then
+  echo
+  echo 'Cloning tmux plugin manager'
+  echo
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  sleep 1s
+fi
